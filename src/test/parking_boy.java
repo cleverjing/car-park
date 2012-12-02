7920d9645245290c22fb242d2949987c48563138
tanjing <tanjing0222@gmail.com>
2012-12-02 16:46:07 +0800

    add parking boy another test case

--- src/test/parking_boy.java
+++ src/test/parking_boy.java
@@ -1,6 +1,8 @@
 package test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Before;
@@ -23,10 +25,20 @@ public class parking_boy {
 	
 	@Test
 	public void parkboy_push_a_car_when_lots_have_space_successly(){
-		boolean result = this.boy.pushACar(this.carNumber, this.carNumber);
+		boolean result = this.boy.pushACar(this.IDCard, this.carNumber);
 		assertTrue("success", result);
 		int number = this.boy.getEmptySpace();
 		assertEquals(number, (this.parkinglots * this.count - 1));
 	}
+	
+	@Test
+	public void parkboy_get_a_car_when_lots_have_a_car_successly(){
+		this.boy.pushACar(this.IDCard, this.carNumber);
+		Object result = this.boy.popAcar(this.IDCard);
+		assertNotNull("Success", result);
+		int count = this.boy.getEmptySpace();
+		assertEquals(count, this.parkinglots * this.count);
+	}
+	
 
 }
